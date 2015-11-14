aac86ce72758ba5302285740e2ee9e91d9d9f29c
Anand <anandps@umich.edu>
2015-11-14 02:28:48 -0500

    Improved code quality

--- src/schemes.py
+++ src/schemes.py
@@ -1,21 +1,25 @@
-from pylab import *
+import numpy as np
 
 def diff(y, u):
-    deta = 1.
-    ny = u.size
-    up = zeros(u.size,dtype = 'complex')
+    deta = 1.0
+    ny = np.size(y)
+    up = np.zeros(ny, dtype = np.complex)
+
     up[1:ny-1] = (u[2:ny] - u[0:ny-2])/(y[2:ny] - y[0:ny-2])
     up[0] = (up[1] - up[0])/(y[1] - y[0])
     up[-1] = (up[-1] - up[-2])/(y[-1] - y[-2])
+
     return up
 
 def diff2(y, u):
-    deta = 1.
-    ny = u.size
-    y_eta = zeros(ny,dtype = 'complex')
-    u_eta2 = zeros(ny,dtype = 'complex')
-    y_eta2 = zeros(ny,dtype = 'complex')
+    deta = 1.0
+    ny = np.size(y)
+    y_eta = np.zeros(ny, dtype = np.complex)
+    u_eta2 = np.zeros(ny, dtype = np.complex)
+    y_eta2 = np.zeros(ny, dtype = np.complex)
+
     u_y = diff(y, u)
+
     y_eta2[1:ny-1] = (y[2:ny] - 2.*y[1:ny-1] + y[0:ny-2])/deta**2
     u_eta2[1:ny-1] = (u[2:ny] - 2.*u[1:ny-1] + u[0:ny-2])/deta**2
     y_eta[1:ny-1] = (y[2:ny] - y[0:ny-2])/(2.*deta)
@@ -30,4 +34,5 @@ def diff2(y, u):
     y_eta[-1] = (y[-1] - y[-2])/deta
     
     u_yy = -(u_y*y_eta2 - u_eta2)/(y_eta**2 + 1.e-100)
+
     return u_yy
