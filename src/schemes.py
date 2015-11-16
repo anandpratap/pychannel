fixed an issue with derivatives at the boundary

src/schemes.py
@@ -6,8 +6,8 @@ def diff(y, u):
     up = np.zeros(ny, dtype = np.complex)
 
     up[1:ny-1] = (u[2:ny] - u[0:ny-2])/(y[2:ny] - y[0:ny-2])
-    up[0] = (up[1] - up[0])/(y[1] - y[0])
-    up[-1] = (up[-1] - up[-2])/(y[-1] - y[-2])
+    up[0] = (u[1] - u[0])/(y[1] - y[0])
+    up[-1] = (u[-1] - u[-2])/(y[-1] - y[-2])
 
     return up
 